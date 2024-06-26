dd900e2f01b562bc17d71692dcbcc8fa688c233f
profelper-dev <profelper@gmail.com>
2024-06-26 10:34:57 +0530

    settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0stvhu4nc0xq@r+5v$+f8vd07#pf!_$l=we!m91priwd=#g-1d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
